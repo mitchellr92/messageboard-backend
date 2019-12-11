4164c205d43d3d084c0d945bbfd4dac9874f5bf4
Mitchell Robles <mitchellr92@live.com>
2019-12-11 14:01:01 -0800

    resloving deployment issues

--- index.js
+++ index.js
@@ -10,11 +10,17 @@ server.use(express.json());
 server.use(cors());
 
 server.get("/", (req, res) => {
-
-})
+  db("posts")
+    .then(post => {
+      res.status(200).json(post);
+    })
+    .catch(err => {
+      res.status(500).json({ message: "Failed to load posts" });
+    });
+});
 
 const PORT = process.env.PORT || 8080;
 
 server.listen(PORT, () => {
-    console.log(`Listening on port ${PORT}`)
+  console.log(`Listening on port ${PORT}`);
 });
